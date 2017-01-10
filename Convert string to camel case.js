53683d749c2420a0efead5c9cb09deaf58b415aa
Stanley <geniustanley@gmail.com>
2017-01-10 21:20:35 +0800

    Convert string to camel case

--- Convert string to camel case.js
+++ Convert string to camel case.js
@@ -1,7 +1,20 @@
 function toCamelCase(str){
-  const regExp = /[-_]\w/ig;
-  return str.replace(regExp, (match) => (
-    match.charAt(1).toUpperCase() 
-  ));
+  return str.split(/-|_/).map( (word, i) => {
+    if (i) {
+      return word.charAt(0).toUpperCase()+word.slice(1); 
+    }
+    return word;
+  }).join('');
 }
+
+/*
+ * best practice *
+-------------------
+  function toCamelCase(str){
+    const regExp = /[-_]\w/ig;
+    return str.replace(regExp, (match) => (
+      match.charAt(1).toUpperCase() 
+    ));
+  }
+*/
 
